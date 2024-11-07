f8386633999ac129bb2e6f2d971a66102ab8a3df
Isa <kmasi4@gmail.com>
2024-11-07 16:24:06 -0300

    Média 2

---
+++ 1003.js
@@ -0,0 +1,5 @@
+var A = parseInt(lines.shift());
+var B = parseInt(lines.shift());
+
+let SOMA = A + B;
+console.log("SOMA = " + SOMA);
---
+++ 1004.js
@@ -0,0 +1,5 @@
+var A = parseInt(lines.shift());
+var B = parseInt(lines.shift());
+
+let PROD = A * B;
+console.log("PROD = " + PROD);
